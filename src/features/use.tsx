921e9c536a260a90c57033d2d241751189184f5c
pool_1541 <pool_1541@hotmail.com>
2024-12-07 13:54:46 -0500

    feat: add use API example

---
+++ src/features/use.tsx
@@ -0,0 +1,34 @@
+import React, { Suspense, use } from 'react';
+import { fetchUsers, UserType } from '../lib';
+import ErrorBoundary from '../lib/error-boundary';
+
+function UsersWrapper({ usersPromise }: { usersPromise: Promise<UserType[]> }) {
+  const users = use(usersPromise);
+
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  );
+}
+
+function Example() {
+  const promise = fetchUsers();
+
+  return (
+    <section>
+      <h2>use API example</h2>
+      <ErrorBoundary fallback={<p>Ha ocurrido un error</p>}>
+        <Suspense fallback={<div>Cargando...</div>}>
+          <UsersWrapper usersPromise={promise} />
+        </Suspense>
+      </ErrorBoundary>
+    </section>
+  );
+}
+
+const ExampleOfUse = React.memo(Example);
+
+export default ExampleOfUse;
