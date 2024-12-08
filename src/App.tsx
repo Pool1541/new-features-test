beff4828418eeaa660d5b9bf47b80ed4078d980d
pool_1541 <pool_1541@hotmail.com>
2024-12-07 19:41:16 -0500

    chore: update App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@
 import './App.css'
 import ExampleOfUse from './features/use'
 import ExampleOfUseActionState from './features/useActionState'
+import ExampleOfUseOptimistic from './features/useOptimistic'
 import ExampleOfUseTransition from './features/useTransition'
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
      <h1>React 19 is here! 🎊</h1>
       {/* <ExampleOfUse /> */}
       {/* <ExampleOfUseTransition /> */}
-      <ExampleOfUseActionState />
+      {/* <ExampleOfUseActionState /> */}
+      <ExampleOfUseOptimistic />
     </main>
   )
 }
